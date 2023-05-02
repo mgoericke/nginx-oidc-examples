b28f030933f8f13bf82be36192702313fa008d97
Arne Hildebrand <hildebrand@rich-and-high.com>
2023-05-02 20:50:52 +0200

    added resource server support in backend

--- src/main/java/de/javamark/demo/oidc/web/SimpleWebController.java
+++ src/main/java/de/javamark/demo/oidc/web/SimpleWebController.java
@@ -1,8 +1,8 @@
 package de.javamark.demo.oidc.web;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,13 +16,14 @@ public class SimpleWebController {
 
     @GetMapping(value = "/example",
             produces = MediaType.TEXT_HTML_VALUE)
-    public String example(@RequestHeader Map<String, String> headers, Model model) {
-        headers.forEach((key, value) -> {
-            log.info(String.format("Header '%s' = %s", key, value));
-        });
-
+    public String example(Authentication authentication, @RequestHeader Map<String, String> headers, Model model) {
         // Füge alle empfangenen Header hinzu
         model.addAttribute("headersMap", headers);
+
+        // Füge alle User-Attributes hinzu
+        OAuth2AuthenticatedPrincipal user = (OAuth2AuthenticatedPrincipal) authentication.getPrincipal();
+        model.addAttribute("userAttributes", user.getAttributes());
+
         return "example";
     }
 }
